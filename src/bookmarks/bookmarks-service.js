031a5aed3cd7a1e9e91c0a7d35f5b5875e723b5b
Evisari Riyanto <evisari111012@gmail.com>
2020-10-30 08:31:04 -0700

    added delete put patch

--- src/bookmarks/bookmarks-service.js
+++ src/bookmarks/bookmarks-service.js
@@ -5,6 +5,21 @@ const BookmarksService = {
   getById(knex, id) {
     return knex.from("items").select("*").where("id", id).first();
   },
+  insertBookmark(knex, newBookmark) {
+    return knex
+      .insert("newBookmark")
+      .into("items")
+      .returning("*")
+      .then((rows) => {
+        return rows[0];
+      });
+  },
+  deleteBookmark(knex, id) {
+    return knex.where({ id }).delete();
+  },
+  updateBookmark(knex, id, newBookmarkFields) {
+    return knex("items").where({ id }).update(newBookmarkFields);
+  },
 };
 
 module.exports = BookmarksService;
